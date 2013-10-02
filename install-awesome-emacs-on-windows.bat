46376dcc536da13e34729933c60b1506182ef440
xgfone <xgfone@126.com>
2013-10-02 10:35:49 +0800

    Correct the echo information when installing on windows.

--- install-awesome-emacs-on-windows.bat
+++ install-awesome-emacs-on-windows.bat
@@ -2,10 +2,10 @@ set source_dir=%~dp0
 set emacs_dir=%USERPROFILE%\AppData\Roaming
 set destination_dir=%emacs_dir%\.dotemacs
 
+echo off
 echo "Installing ..."
-
 if not exist "%destination_dir%" (mkdir "%destination_dir%")
 xcopy /S /Q /Y "%source_dir%*"  "%destination_dir%"
 copy "%destination_dir%\.emacs" "%emacs_dir%\.emacs"
-
 echo "Install Successfully! :-)\n"
+echo on
--- install-simple-emacs-on-windows.bat
+++ install-simple-emacs-on-windows.bat
@@ -1,5 +1,6 @@
 set DIRNAME=%~dp0
+echo off
 echo "Installing ...\n"
 copy  "%DIRNAME%configs\emacs-basic-config.el"  "%USERPROFILE%\AppData\Roaming\.emacs"
-
 echo "Install Successfully! :-)\n"
+echo on
