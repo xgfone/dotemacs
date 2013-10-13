9c87cf4745059cd336a788e2b86bfa125e084684
xgfone <xgfone@126.com>
2013-10-13 14:05:40 +0800

    Modify the ecb's configuration.
    Cancel the tip each day and the upgrade check, and so on.

--- configs/ecb-config.el
+++ configs/ecb-config.el
@@ -3,9 +3,12 @@
 (add-to-list 'load-path "~/.dotemacs/lisps/ecb/")
 (require 'ecb)
 
-(ecb-minor-mode t)
-(ecb-minor-menu t)
-
 ;; close the tip when ecb starts each time.
 (setq ecb-tip-of-the-day nil)
+(setq ecb-upgrade-check-done nil)
+(setq ecb-auto-compatibility-check nil)
 (setq ecb-primary-secondary-mouse-buttons (quote mouse-1--mouse-2))
+
+;;(ecb-minor-mode t)
+;;(ecb-minor-menu t)
+
